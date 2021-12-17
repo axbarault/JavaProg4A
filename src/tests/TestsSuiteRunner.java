2e2369899b15a90934601f2e6ebd2c09eb0f2505
Axel Barault <axbarault@etud.univ-angers.fr>
2021-12-17 16:19:02 +0100

    Commentaires++

--- src/tests/TestsSuiteRunner.java
+++ src/tests/TestsSuiteRunner.java
@@ -9,11 +9,13 @@ public class TestsSuiteRunner {
 	public static void main(String[] args) {
 
 		Result result = JUnitCore.runClasses(TestsSuite.class);
-		for (Failure fail : result.getFailures()) 
+		
+		for (Failure fail : result.getFailures()) // Affiche les problèmess 
 		{
 			System.out.println(fail.toString());
 		}
-		if (result.wasSuccessful()) 
+		
+		if (result.wasSuccessful()) // Si aucun problème 
 		{
 			System.out.println("All tests finished successfully...");
 		}
